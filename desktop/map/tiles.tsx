49c2e85f1bf26437c22ed648269f2c238ce482d9
Minuscorgiee <85828878+Minuscorgiee@users.noreply.github.com>
2022-07-01 18:55:35 +0200

    pliki mapy

---
+++ desktop/map/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tiles" tilewidth="64" tileheight="64" tilecount="720" columns="60">
+ <image source="tiles.png" width="3840" height="768"/>
+</tileset>
